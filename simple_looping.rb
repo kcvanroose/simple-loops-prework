67c0dd7885326a835ee227887c03dc159d3b4337
casey van-roose <kcvanroose@gmail.com>
2018-06-29 14:46:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -44,7 +44,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number in number_of_times
+    number_of_times.each 
     puts phrase
   end
 end
